6467249d501c6490440ec35edcd19b7576cc1272
Saima Fathima <saima.fathi2011@gmail.com>
2024-12-16 17:23:12 +0530

    avg

---
+++ Avg.py
@@ -0,0 +1,9 @@
+tree1=98
+tree2=94
+tree3=76
+tree4=54
+tree5=32
+sum=tree1+tree2+tree3+tree4+tree5
+print("the sum of all 5 trees are",sum )
+average=sum/5
+print("the avg of all trees is", average)
