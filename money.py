db58f7de21c5bcf0c8b77566bf10bc8e4a73f610
Saima Fathima <saima.fathi2011@gmail.com>
2024-12-16 17:33:07 +0530

    money

--- money.py
+++ money.py
@@ -1,7 +1,7 @@
 amount=int(input("please enter the amount for withdraw"))
 note_1=amount//100
 note_2=(amount%100)//50
-note_3=((amount%100)//50)//10
+note_3=((amount%100)%50)//10
 print("note of 100 rupees",note_1)
 print("note of 100 rupees",note_2)
 print("note of 10 rupees",note_3)
