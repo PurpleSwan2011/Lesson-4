8ae55260fe0e9dd2d307c7d34cd43cd2f7b69250
Saima Fathima <saima.fathi2011@gmail.com>
2024-12-16 17:40:12 +0530

    perc

---
+++ percentage.py
@@ -0,0 +1,10 @@
+print("enter marks obtained in 4 subs:")
+math=int(input("maths :"))
+eng=int(input("eng :"))
+hin=int(input("hin :"))
+kan=int(input("kan :"))
+sum=math+eng+kan+hin
+print("sum of eng,math,kan and hin")
+perc = (sum/400)*100
+print(end="percentage=")
+print(perc)
