b7103275d588e7c7b2e1453004a7590c3d2224a4
Saima Fathima <saima.fathi2011@gmail.com>
2024-12-17 16:37:38 +0530

    d

---
+++ descending.py
@@ -0,0 +1,8 @@
+i=int(input("enter a no:"))
+if (i<15):
+    print("i is smaller than 15")
+    print("i am in if block")
+else:
+    print("i is greater than 15")
+    print("i am in else block")
+print("i am in if and not in else")
