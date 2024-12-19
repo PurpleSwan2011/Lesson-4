bdf579add833617ac7c92c6155a14b083d1da865
Saima Fathima <saima.fathi2011@gmail.com>
2024-12-19 16:27:00 +0530

    odd or and

---
+++ odd or and.py
@@ -0,0 +1,11 @@
+a=10
+b=-10
+c=0 
+if a>0 or b>0:
+    print("either of the no is greater than 0")
+else:
+    ("no other no is greater than 0")
+if b>0 or c>0:
+     print("either of the no is greater than 0")
+else:
+    ("no other no is greater than 0")
