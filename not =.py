50daf3b1a079cc77a0313f989aede2191e23783f
Saima Fathima <saima.fathi2011@gmail.com>
2024-12-19 16:36:31 +0530

    not=

---
+++ not =.py
@@ -0,0 +1,16 @@
+a=10 
+b=12 
+c=12 
+print(a!=b)
+print(b!=c)
+a="python"
+b="coding"
+if a!=b:
+    print(a,'and',b,'are different')
+a=4 
+b=5 
+if (a==b) != (b==c):
+    print('hello')
+a=int(input("enter a no"))
+if a%2 != 0:
+    print(a,"is not even no")
