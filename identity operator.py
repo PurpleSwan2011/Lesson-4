e9e6ed939e76b16e2310fab55f8e2fccf531a593
Saima Fathima <saima.fathi2011@gmail.com>
2024-12-23 16:26:53 +0530

    io

---
+++ identity operator.py
@@ -0,0 +1,16 @@
+x=5
+if (type(x)is int):
+    print("true")
+else:
+    print("false")
+x=4.0 
+if (type(x)is float):
+    print("true")
+else:
+    print("false")
+x=20
+y=20
+if(x is y):
+    print("x & y are same identities")
+else:
+    print("x & y are different identities")
