abdd7fdd26b8a70e9abff0d5b7d65c538a7b7b01
Saima Fathima <saima.fathi2011@gmail.com>
2024-12-27 11:29:10 +0530

    p

---
+++ p3.py
@@ -0,0 +1,55 @@
+x=input("enter the ASCII code:")
+if x=="65":
+    print("ASCII code stands 65 for alphabet A")
+elif x=="66":
+    print("ASCII code stands 66 for alphabet B")
+elif x=="67":
+    print("ASCII code stands 67 for alphabet C")
+elif x=="68":
+    print("ASCII code stands 68 for alphabet D")
+elif x=="69":
+    print("ASCII code stands 69 for alphabet E")
+elif x=="70":
+    print("ASCII code stands 70 for alphabet F")
+elif x=="71":
+    print("ASCII code stands 71 for alphabet G")
+elif x=="72":
+    print("ASCII code stands 72 for alphabet H")
+elif x=="73":
+    print("ASCII code stands 73 for alphabet I")
+elif x=="74":
+    print("ASCII code stands 74 for alphabet J")
+elif x=="75":
+    print("ASCII code stands 75 for alphabet K")
+elif x=="76":
+    print("ASCII code stands 66 for alphabet L")
+elif x=="77":
+    print("ASCII code stands 66 for alphabet M")
+elif x=="78":
+    print("ASCII code stands 66 for alphabet N")
+elif x=="79":
+    print("ASCII code stands 66 for alphabet O")
+elif x=="80":
+    print("ASCII code stands 66 for alphabet P")
+elif x=="81":
+    print("ASCII code stands 66 for alphabet Q")
+elif x=="82":
+    print("ASCII code stands 66 for alphabet R")
+elif x=="83":
+    print("ASCII code stands 66 for alphabet S")
+elif x=="84":
+    print("ASCII code stands 66 for alphabet T")
+elif x=="85":
+    print("ASCII code stands 66 for alphabet U")
+elif x=="86":
+    print("ASCII code stands 66 for alphabet V")
+elif x=="87":
+    print("ASCII code stands 66 for alphabet W")
+elif x=="88":
+    print("ASCII code stands 66 for alphabet X")
+elif x=="89":
+    print("ASCII code stands 66 for alphabet Y")
+elif x=="90":
+    print("ASCII code stands 66 for alphabet Z")
+else:
+    print("pls enter valid option")
