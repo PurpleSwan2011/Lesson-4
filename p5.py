d0c56fabe0b73e91c733f63c3f7c27809576e50b
Saima Fathima <saima.fathi2011@gmail.com>
2024-12-27 11:34:37 +0530

    p

---
+++ p5.py
@@ -0,0 +1,5 @@
+age=int(input("how old are u?:"))
+if age>10 and age<20:
+    print("u are allowed in class age limit in b/w 10 and 20")
+elif age<10 and age>20:
+    print("u are not allowed in class age limit in b/w 10 and 20")
